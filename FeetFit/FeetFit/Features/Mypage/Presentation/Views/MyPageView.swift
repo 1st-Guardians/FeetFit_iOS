ae4083d2cb517e38d5e19a4ce0276f67d9de1192
alwn8918 <alwn8918@gmail.com>
2026-05-18 14:24:39 +0900

    feat: TabView 구현

---
+++ FeetFit/FeetFit/Features/Mypage/Presentation/Views/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  FeetFit
+//
+//  Created by 김미주 on 5/18/26.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text("MyPage")
+    }
+}
+
+#Preview {
+    MyPageView()
+}
