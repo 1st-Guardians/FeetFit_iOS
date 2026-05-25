f3114652b433e74882f250e56ceb85aa15f9999a
codmsdlrltgjqm <chaeni1218@naver.com>
2026-05-25 18:44:14 +0900

    refactor: 로딩 메시지 UI 수정

--- FeetFit/FeetFit/Features/Onboarding/Presentation/Views/HardwarePairingFinishView.swift
+++ FeetFit/FeetFit/Features/Onboarding/Presentation/Views/HardwarePairingFinishView.swift
@@ -13,15 +13,9 @@ struct HardwarePairingFinishView: View {
     var body: some View {
         VStack(spacing: 0) {
             
-            LottieView(animation: .named("FeetFit Loading", bundle: Bundle.main))
-                .playing(loopMode: .loop)
-                .frame(width: 100, height: 100)
-                .padding(.bottom, 10)
-                .padding(.top, 217)
-            
-            Text("하드웨어 연결이 완료되었어요\n핏핏으로 가볼까요?")
-                .pretendardFont(.SubTitle)
-                .multilineTextAlignment(.center)
+            LoadingMessageView(
+                message: "하드웨어 연결이 완료되었어요\n핏핏으로 가볼까요?"
+            )
             
             Spacer()
         }
--- FeetFit/FeetFit/Features/Onboarding/Presentation/Views/HardwarePairingView.swift
+++ FeetFit/FeetFit/Features/Onboarding/Presentation/Views/HardwarePairingView.swift
@@ -13,15 +13,9 @@ struct HardwarePairingView: View {
     var body: some View {
         VStack(spacing: 0) {
             
-            LottieView(animation: .named("FeetFit Loading", bundle: Bundle.main))
-                .playing(loopMode: .loop)
-                .frame(width: 100, height: 100)
-                .padding(.bottom, 10)
-                .padding(.top, 217)
-            
-            Text("하드웨어를\n연결 중이에요.")
-                .pretendardFont(.SubTitle)
-                .multilineTextAlignment(.center)
+            LoadingMessageView(
+                message: "하드웨어를\n연결 중이에요."
+            )
             
             Spacer()
         }
